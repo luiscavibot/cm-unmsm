c3a6024c237858dc4e636ea92495b38cd14d9a9b
Luis Castillo <lcastillov123@gmail.com>
2022-02-28 22:36:14 -0500

    back de testimonios mejorado

---
+++ src/api/facultad/routes/facultad.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * facultad router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::facultad.facultad');
---
+++ src/api/facultad/services/facultad.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * facultad service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::facultad.facultad');
